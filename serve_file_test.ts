4bdb45513cb3ba19a2bcbad9046c31e55603b4ab
nekojanai <nekojanai@debnyan.com>
2022-02-07 23:03:45 +0100

    chore: adds more tests
    refactor: Tests now use Status and STATUS_TEXT from stdlib

--- serve_file_test.ts
+++ serve_file_test.ts
@@ -1,5 +1,5 @@
 import { serveFile } from "./serve_file.ts";
-import { assertEquals, readerFromStreamReader } from "./test_deps.ts";
+import { assertEquals, readerFromStreamReader, Status, STATUS_TEXT } from "./test_deps.ts";
 
 const { test } = Deno;
 
@@ -13,7 +13,11 @@ test("Serve image file", async function (t) {
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of image/jpg", function () {
@@ -46,7 +50,11 @@ test("Serve a file with spaces", async function (t) {
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of text/plain; charset=utf8", function() {
@@ -74,8 +82,12 @@ test("Serve an non existant file", async function (t) {
     new Request("https://test.local/test_data/images/aaaaa.jpg")
   );
 
-  await t.step("respond with a 404", function () {
-    assertEquals(response.status, 404);
+  await t.step("responds with 404", function () {
+    assertEquals(response.status, Status.NotFound);
+  });
+
+  await t.step("responds with status text Not Found", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.NotFound));
   });
 
   await t.step("body is null", function () {
@@ -95,7 +107,11 @@ test("Serve a file from a specified directory", async function (t) {
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of text/css; charset=utf8", function() {
@@ -130,7 +146,11 @@ test("Serve a file from a specified directory, with last part of the servePath m
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of text/css; charset=utf8", function() {
@@ -160,8 +180,12 @@ test("Serve a file from a specified directory with the request path matching the
     }
   );
 
-  await t.step("respond with a 404", function () {
-    assertEquals(response.status, 404);
+  await t.step("responds with 404", function () {
+    assertEquals(response.status, Status.NotFound);
+  });
+
+  await t.step("responds with status text Not Found", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.NotFound));
   });
 
   await t.step("body is null", function () {
@@ -183,7 +207,11 @@ test("Serve a file with allowed extensions specified", async function (t) {
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of text/plain; charset=utf8", function() {
@@ -213,8 +241,12 @@ test("Serve a file with an extension not in the list of allowed extensions", asy
     }
   );
 
-  await t.step("respond with a 403", function () {
-    assertEquals(response.status, 403);
+  await t.step("responds with 403", function () {
+    assertEquals(response.status, Status.Forbidden);
+  });
+
+  await t.step("responds with status text Forbidden", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.Forbidden));
   });
 
   await t.step("body is null", function () {
@@ -227,8 +259,12 @@ test("Serve a hidden file with serveHiddenFiles false", async function (t) {
     new Request("https://test.local/test_data/.hidden_file.txt")
   );
 
-  await t.step("respond with a 403", function () {
-    assertEquals(response.status, 403);
+  await t.step("responds with 403", function () {
+    assertEquals(response.status, Status.Forbidden);
+  });
+
+  await t.step("responds with status text Forbidden", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.Forbidden));
   });
 
   await t.step("body is null", function () {
@@ -248,7 +284,11 @@ test("Serve a hidden file with serveHiddenFiles true", async function (t) {
   );
 
   await t.step("responds with 200", function () {
-    assertEquals(response.status, 200);
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
   });
 
   await t.step("has content-type header of text/plain; charset=utf8", function() {
@@ -269,4 +309,114 @@ test("Serve a hidden file with serveHiddenFiles true", async function (t) {
   });
 
   fixture.close();
+});
+
+test("Serve a hidden file with serveHiddenFiles true, with from a specified directory, with an allowed file extension", async function (t) {
+  const fixture = await Deno.open("./test_data/.hidden_file.txt");
+  const fixtureInfo = await fixture.stat();
+  const fixtureData = new Uint8Array(fixtureInfo.size);
+  await fixture.read(fixtureData);
+  const response = serveFile(
+    new Request("https://test.local/test_data/.hidden_file.txt"), {
+      servePath: "./test_data/",
+      allowedExtensions: [".txt"],
+      serveHiddenFiles: true
+    }
+  );
+
+  await t.step("responds with 200", function () {
+    assertEquals(response.status, Status.OK);
+  });
+
+  await t.step("responds with status text OK", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.OK));
+  });
+
+  await t.step("has content-type header of text/plain; charset=utf8", function() {
+    const contentType = response.headers.get("content-type");
+    assertEquals(contentType, "text/plain; charset=utf-8")
+  });
+
+  await t.step("has content-length header equal to file size", function () {
+    const contentLength = parseInt(response.headers.get("content-length")!);
+    assertEquals(contentLength, fixtureInfo.size);
+  });
+
+  await t.step("body is actual file", async function () {
+    const contentLength = response.headers.get("content-length");
+    const p = new Uint8Array(parseInt(contentLength!));
+    await readerFromStreamReader(response.body?.getReader()!).read(p);
+    assertEquals(p, fixtureData);
+  });
+
+  fixture.close();
+});
+
+test("Serve a hidden file with serveHiddenFiles false, with a specified directory, with an allowed file extension", async function (t) {
+  const response = serveFile(
+    new Request("https://test.local/test_data/.hidden_file.txt"), {
+      servePath: "./test_data/",
+      allowedExtensions: [".txt"],
+      serveHiddenFiles: false
+    }
+  );
+
+  await t.step("responds with 403", function () {
+    assertEquals(response.status, Status.Forbidden);
+  });
+
+  await t.step("responds with status text Forbidden", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.Forbidden));
+  });
+
+  await t.step("body is null", function () {
+    assertEquals(response.body, null);
+  });
+
+});
+
+test("Serve a hidden file with serveHiddenFiles true, with a specified directory with more than the last part matching, with an allowed file extension", async function (t) {
+  const response = serveFile(
+    new Request("https://test.local/test_data/test_subdir/.hidden_file.txt"), {
+      servePath: "./test_data/test_subdir/",
+      allowedExtensions: [".txt"],
+      serveHiddenFiles: true
+    }
+  );
+
+  await t.step("responds with 404", function () {
+    assertEquals(response.status, Status.NotFound);
+  });
+
+  await t.step("responds with status text Not Found", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.NotFound));
+  });
+
+  await t.step("body is null", function () {
+    assertEquals(response.body, null);
+  });
+
+});
+
+test("Serve a hidden file with serveHiddenFiles true, with a specified directory, with a forbidden file extension", async function (t) {
+  const response = serveFile(
+    new Request("https://test.local/test_data/.hidden_file.txt"), {
+      servePath: "./test_data/",
+      allowedExtensions: [".png"],
+      serveHiddenFiles: true
+    }
+  );
+
+  await t.step("responds with 403", function () {
+    assertEquals(response.status, Status.Forbidden);
+  });
+
+  await t.step("responds with status text Forbidden", function () {
+    assertEquals(response.statusText, STATUS_TEXT.get(Status.Forbidden));
+  });
+
+  await t.step("body is null", function () {
+    assertEquals(response.body, null);
+  });
+
 });
--- test_deps.ts
+++ test_deps.ts
@@ -1,2 +1,3 @@
 export { assertEquals } from "https://deno.land/std@0.123.0/testing/asserts.ts";
 export { readerFromStreamReader } from "https://deno.land/std@0.123.0/streams/conversion.ts";
+export { Status, STATUS_TEXT } from "https://deno.land/std@0.125.0/http/http_status.ts";
